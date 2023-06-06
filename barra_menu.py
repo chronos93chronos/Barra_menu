674bb1126fd83f5cbacf1fe4dad7e08481c46e40
ALEXANDER HERNANDEZ <111362778+chronos93chronos@users.noreply.github.com>
2023-06-06 17:54:18 -0400

    Add files via upload
    Actualizacion

--- barra_menu.py
+++ barra_menu.py
@@ -8,13 +8,33 @@ ventana.geometry("300x400")
 ventana.title("BARRA MENU")
 ventana.resizable(False, False)
 #__________________________________________________________
+#FUNCIONES 
+def saludo ():
+    print("HELLO USER ACTIVE")
+
+
+#__________________________________________________________
 
 barra_menu = tk.Menu()
 menu_archivo = tk.Menu(barra_menu, tearoff=False)
-barra_menu.add_cascade(menu=menu_archivo, label="Archivo")
+img_menu_nuevo = tk.PhotoImage(file="logo.png")
 
+barra_menu.add_cascade(menu=menu_archivo, label="ARCHIVO")
+menu_archivo.add_command(
+    label="Nuevo",
+    accelerator="Ctrl+N",
+    command= saludo,
+    )
 
+menu_archivo.add_separator()#sirve para poner una linea de separacion en el menu cascada
 
+menu_archivo.add_command(label="Ayuda")
+
+#-------------
+menu_herramientas = tk.Menu(barra_menu, tearoff=False)
+barra_menu.add_cascade(menu=menu_herramientas, label="HERRAMIENTAS")
+menu_herramientas.add_command(label="Informacion")
+menu_herramientas.add_command(label="FAQ")
 
 
 
